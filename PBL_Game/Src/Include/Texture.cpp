2d478460b4c08e5c951e7e3f4ae0e137104f5fda
skowrons <Pawel.Skowronski@tomtom.com>
2019-05-03 22:39:03 +0200

    Simple Textures class

--- PBL_Game/Src/Include/Texture.cpp
+++ PBL_Game/Src/Include/Texture.cpp
@@ -27,44 +27,32 @@ bool Texture::Load()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, mipmap_param);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mipmap_param);
 	// load image, create texture and generate mipmaps
-	int width, height, nrChannels, components_per_pixel;
-	// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-
+	int width, height, components_per_pixel;
 	unsigned char *data = nullptr;
-	unsigned char *embdata = nullptr;
 	aiTexture *embededData = nullptr;
+	unsigned format = 0;
 
 	if (textures != nullptr)
 	{
 
 		embededData = textures[(int)(path.c_str()[1] - '0')];
-		embdata = stbi_load_from_memory(reinterpret_cast<unsigned char *>(embededData->pcData), embededData->mWidth, &width, &height, &components_per_pixel, 0);
+		data = stbi_load_from_memory(reinterpret_cast<unsigned char *>(embededData->pcData), embededData->mWidth, &width, &height, &components_per_pixel, 0);
 	}
 	else
 	{
-		data = stbi_load(path.c_str(), &width, &height, &nrChannels, STBI_rgb_alpha);
+		data = stbi_load(path.c_str(), &width, &height, &components_per_pixel, STBI_rgb_alpha);
 	}
 
-	if (data != nullptr || embededData != nullptr)
+	if (components_per_pixel == 1)
+		format = GL_RED;
+	else if (components_per_pixel == 3)
+		format = GL_RGB;
+	else if (components_per_pixel == 4)
+		format = GL_RGBA;
+
+	if (data != nullptr)
 	{
-		if (embededData != nullptr)
-		{
-			
-			if (components_per_pixel == 3)
-			{
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, embdata);
-			}
-			else if (components_per_pixel == 4)
-			{
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, embdata);
-			}
-		}
-		else
-		{
-
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		}
-
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 	else
