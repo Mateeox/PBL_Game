699c647e960b21381fdf979207f549b0597a9d9c
Mateox <mat.lapies@gmail.com>
2019-05-14 18:29:28 +0200

    MapElement reimplementation started

--- PBL_Game/Src/Include/Component/MapElement.cpp
+++ PBL_Game/Src/Include/Component/MapElement.cpp
@@ -1,8 +1,9 @@
 #include "MapElement.hpp"
 
-MapElement::MapElement(glm::vec2 pos)
+MapElement::MapElement(glm::vec2 pos, Shader* shaderProgram)
 {
-	Position = pos;
+	this.Position = pos;
+	this->shader = shaderProgram;
 }
 
 void MapElement::SetWall(glm::vec4 wall)
@@ -13,4 +14,43 @@ void MapElement::SetWall(glm::vec4 wall)
 void MapElement::SetDoor(glm::vec4 door)
 {
 	Doors += door;
+}
+
+std::vector<glm::vec2> MapElement::GetNeighbours()
+{
+	std::vector<glm::vec2> neighbours;
+	neighbours.push_back(this.Position + glm::vec2(1.0f, 0));
+	neighbours.push_back(this.Position + glm::vec2(-1.0f, 0));
+	neighbours.push_back(this.Position + glm::vec2(0, 1.0f));
+	neighbours.push_back(this.Position + glm::vec2(0, -1.0f));
+	return neighbours;
+}
+
+SceneNode* MapElement::GenerateNode(std::vector<SceneNode>* nodes, SceneNode* parent)
+{
+	SceneNode element;
+	
+}
+
+SceneNode* MapElement::AddFloor()
+{
+	return nullptr;
+}
+
+SceneNode* MapElement::CreateWall(SceneNode* parent, float direction_x, float direction_y)
+{
+	return nullptr;
+}
+
+SceneNode* MapElement::CreateDoor(SceneNode* parent, float direction_x, float direction_y)
+{
+	return nullptr;
+}
+
+void MapElement::AddWalls(SceneNode* node)
+{
+}
+
+void MapElement::AddDoors(SceneNode* node)
+{
 }
