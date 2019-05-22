10d42f00799a29a418b54b8dc47216a85cc6aaa9
Mateox <mat.lapies@gmail.com>
2019-05-22 02:24:57 +0200

    Walls creation - further implementation needed

--- PBL_Game/Src/Include/Component/MapElement.cpp
+++ PBL_Game/Src/Include/Component/MapElement.cpp
@@ -56,7 +56,6 @@ SceneNode* MapElement::AddFloor()
 		xD);
 	oFloor->AddComponent(Floor);
 	floor->AddGameObject(oFloor);
-	floor->AddChild(floor);
 	floor->Translate(Position.x, Position.y, 0);
 	nodes->push_back(floor);
 	return floor;
@@ -64,7 +63,21 @@ SceneNode* MapElement::AddFloor()
 
 SceneNode* MapElement::CreateWall(SceneNode* parent, float direction_x, float direction_y)
 {
-	return nullptr;
+	SceneNode* wall = new SceneNode();
+	GameObject* wallObj = new GameObject(wall->local);
+	Texture* xD = new Texture("Textures/red.png", GL_LINEAR);
+	xD->Load();
+	ShapeRenderer3D *szescian = new ShapeRenderer3D(Shapes::RainBow_Cube,
+		Shapes::RB_Cube_indices,
+		sizeof(Shapes::RainBow_Cube),
+		sizeof(Shapes::RB_Cube_indices),
+		*shader,
+		xD);
+	wallObj->AddComponent(szescian);
+	wall->AddGameObject(wallObj);
+	wall->Translate(Position.x + direction_x * wall_offset, Position.y + direction_y * wall_offset, 0);
+	wall->AddParent(parent);
+	return wall;
 }
 
 SceneNode* MapElement::CreateDoor(SceneNode* parent, float direction_x, float direction_y)
@@ -74,8 +87,24 @@ SceneNode* MapElement::CreateDoor(SceneNode* parent, float direction_x, float di
 
 void MapElement::AddWalls(SceneNode* node)
 {
+	if (Walls.x > 0)
+		this->CreateWall(node, 0, 1.0f);
+	if (Walls.y > 0)
+		this->CreateWall(node, 1.0f, 0);
+	if (Walls.z > 0)
+		this->CreateWall(node, 0, -1.0f);
+	if (Walls.w > 0)
+		this->CreateWall(node, -1.0f, 0);
 }
 
 void MapElement::AddDoors(SceneNode* node)
 {
+	if (Doors.x > 0)
+		this->CreateWall(node, 0, 1.0f);
+	if (Doors.y > 0)
+		this->CreateWall(node, 1.0f, 0);
+	if (Doors.z > 0)
+		this->CreateWall(node, 0, -1.0f);
+	if (Doors.w > 0)
+		this->CreateWall(node, -1.0f, 0);
 }
