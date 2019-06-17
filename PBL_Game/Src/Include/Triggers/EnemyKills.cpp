4aba32b555bb40a8ddbabc6b17427579aac2f98b
pawcijo <apanapantesz@o2.pl>
2019-06-17 18:26:03 +0200

    ovverride trigger

--- PBL_Game/Src/Include/Triggers/EnemyKills.cpp
+++ PBL_Game/Src/Include/Triggers/EnemyKills.cpp
@@ -5,3 +5,13 @@ Trigger(transform),background(aBack),lostScreen(aLost)
 {
 	this->Player = player;
 }
+
+bool EnemyKills::checkCollision(Collider* other)
+{
+	glm::vec3 translatedCoords = coords + transform.getPosition() * transform.getScale();
+	glm::vec3 otherTranslatedCoords = other->coords + other->transform.getPosition() * other->transform.getScale();
+	return
+		(translatedCoords.x <= otherTranslatedCoords.x + other->dimensions.x) && (translatedCoords.x + dimensions.x >= otherTranslatedCoords.x) &&
+		(translatedCoords.y <= otherTranslatedCoords.y + other->dimensions.y) && (translatedCoords.y + dimensions.y >= otherTranslatedCoords.y) &&
+		(translatedCoords.z <= otherTranslatedCoords.z + other->dimensions.z) && (translatedCoords.z + dimensions.z >= otherTranslatedCoords.z);
+}
