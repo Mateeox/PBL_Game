b3f9457b2a730c7f40ff82290d6ca4f69c3ad702
pawcijo <apanapantesz@o2.pl>
2019-06-18 01:01:03 +0200

    change logic of collider to more simple

--- PBL_Game/Src/Include/PlayerCollider.cpp
+++ PBL_Game/Src/Include/PlayerCollider.cpp
@@ -8,10 +8,39 @@ PlayerCollider::PlayerCollider(Game * aGame,Transform & transform) : Collider(tr
 
 bool PlayerCollider::checkCollision(Collider* other)
 {
-	if (Enabled) {
+	if (Enabled) 
+	{
 
 
-		if (other->gameobject->getTag() != "enemy" )
+		if (other->gameobject->getTag() == "enemy" )
+		{
+			glm::vec3 translatedCoords = coords + transform.getPosition() * transform.getScale();
+			glm::vec3 otherTranslatedCoords = other->coords + other->transform.getPosition() * other->transform.getScale();
+			  if (
+				(translatedCoords.x <= otherTranslatedCoords.x + other->dimensions.x) && (translatedCoords.x + dimensions.x >= otherTranslatedCoords.x) &&
+				(translatedCoords.y <= otherTranslatedCoords.y + other->dimensions.y) && (translatedCoords.y + dimensions.y >= otherTranslatedCoords.y) &&
+				(translatedCoords.z <= otherTranslatedCoords.z + other->dimensions.z) && (translatedCoords.z + dimensions.z >= otherTranslatedCoords.z))
+			  {
+				  if (game->EnemyOnLefSide && gameobject->getTag() == "leftPlayer")
+				  {
+				  	  return true;
+				  }
+				  else if (!game->EnemyOnLefSide && gameobject->getTag() == "rightPlayer")
+				  {
+				  	  return true;
+				  }
+				  else
+				  {
+				  	  return false;
+				  }
+
+			  }
+			  else
+			  {
+			    	return false;
+			  }
+		}
+		else
 		{
 			glm::vec3 translatedCoords = coords + transform.getPosition() * transform.getScale();
 			glm::vec3 otherTranslatedCoords{ 0 };
@@ -29,37 +58,10 @@ bool PlayerCollider::checkCollision(Collider* other)
 				return false;
 			}
 		}
-		else
-		{
 
-			glm::vec3 translatedCoords = coords + transform.getPosition() * transform.getScale();
-			glm::vec3 otherTranslatedCoords = other->coords + other->transform.getPosition() * other->transform.getScale();
-			if (
-				(translatedCoords.x <= otherTranslatedCoords.x + other->dimensions.x) && (translatedCoords.x + dimensions.x >= otherTranslatedCoords.x) &&
-				(translatedCoords.y <= otherTranslatedCoords.y + other->dimensions.y) && (translatedCoords.y + dimensions.y >= otherTranslatedCoords.y) &&
-				(translatedCoords.z <= otherTranslatedCoords.z + other->dimensions.z) && (translatedCoords.z + dimensions.z >= otherTranslatedCoords.z))
-			{
-				if (game->EnemyOnLefSide && gameobject->getTag() == "leftPlayer")
-				{
-					return true;
-				}
-				else if (!game->EnemyOnLefSide && gameobject->getTag() == "rightPlayer")
-				{
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-				
-			}
-			else
-			{
-				return false;
-			}
-		}
 	}
-	else {
+	else 
+	{
 		return false;
 	}
 
